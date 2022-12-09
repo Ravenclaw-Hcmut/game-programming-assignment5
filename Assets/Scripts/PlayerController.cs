749fa6a70c787494a474cc951a62fa8ea79d4d05
Luan Huynh <luanhuynh2001@gmail.com>
2022-12-10 02:59:43 +0700

    modifying shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,23 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
+using UnityEngine.UI;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class PlayerController : MonoBehaviour
+
+//public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks
 {
     [SerializeField] GameObject cameraHolder;
 
     [SerializeField] float mouseSensitivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
+
+    [SerializeField] Item[] items;
+
+    int itemIndex;
+    int previousItemIndex = -1;
 
     float verticalLookRotation;
     bool grounded;
@@ -27,10 +37,21 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-        if (!pv.IsMine)
+        //if (!pv.IsMine)
+        //{
+        //    Destroy(GetComponentInChildren<Camera>().gameObject);
+        //    Destroy(rb);
+        //}
+
+        if (pv.IsMine)
+        {
+            EquipItem(0);
+        }
+        else
         {
             Destroy(GetComponentInChildren<Camera>().gameObject);
             Destroy(rb);
+            //Destroy(ui);
         }
     }
 
@@ -40,6 +61,49 @@ public class PlayerController : MonoBehaviour
         Look();
         Move();
         Jump();
+
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (Input.GetKeyDown((i + 1).ToString()))
+            {
+                EquipItem(i);
+                break;
+            }
+        }
+
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
+        {
+            if (itemIndex >= items.Length - 1)
+            {
+                EquipItem(0);
+            }
+            else
+            {
+                EquipItem(itemIndex + 1);
+            }
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0f)
+        {
+            if (itemIndex <= 0)
+            {
+                EquipItem(items.Length - 1);
+            }
+            else
+            {
+                EquipItem(itemIndex - 1);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            items[itemIndex].Use();
+        }
+
+        if (transform.position.y < -10f) // Die if you fall out of the world
+        {
+            //Die();
+        }
     }
 
     public void SetGroundedState(bool __grounded)
@@ -56,13 +120,47 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        Debug.Log("Keydown: " + Input.GetKeyDown(KeyCode.Space));
-        Debug.Log("is grounded: " + grounded);
+        //Debug.Log("Keydown: " + Input.GetKeyDown(KeyCode.Space));
+        //Debug.Log("is grounded: " + grounded);
         if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             rb.AddForce(transform.up * jumpForce);
         }
     }
+
+
+    void EquipItem(int _index)
+    {
+        if (_index == previousItemIndex)
+            return;
+
+        itemIndex = _index;
+
+        items[itemIndex].itemGameObject.SetActive(true);
+
+        if (previousItemIndex != -1)
+        {
+            items[previousItemIndex].itemGameObject.SetActive(false);
+        }
+
+        previousItemIndex = itemIndex;
+
+        if (pv.IsMine)
+        {
+            Hashtable hash = new Hashtable();
+            hash.Add("itemIndex", itemIndex);
+            PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+        }
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        if (changedProps.ContainsKey("itemIndex") && !pv.IsMine && targetPlayer == pv.Owner)
+        {
+            EquipItem((int)changedProps["itemIndex"]);
+        }
+    }
+
 
     void Look()
     {
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
 using System.IO;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class PlayerManager : MonoBehaviour
 {
     PhotonView pv;
 
-    private void Awake()
+	GameObject controller;
+
+	int kills;
+	int deaths;
+
+	private void Awake()
     {
         pv = GetComponent<PhotonView>();
     }
@@ -32,5 +40,41 @@ public class PlayerManager : MonoBehaviour
     void CreateController()
     {
         PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
-    }
+		//Transform spawnpoint = SpawnManager.Instance.GetSpawnpoint();
+		//controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnpoint.position, spawnpoint.rotation, 0, new object[] { PV.ViewID });
+	}
+
+
+	public void Die()
+	{
+		//PhotonNetwork.Destroy(controller);
+		//CreateController();
+
+		//deaths++;
+
+		//Hashtable hash = new Hashtable();
+		//hash.Add("deaths", deaths);
+		//PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+	}
+
+
+	public void GetKill()
+	{
+		//PV.RPC(nameof(RPC_GetKill), PV.Owner);
+	}
+
+	[PunRPC]
+	void RPC_GetKill()
+	{
+		//kills++;
+
+		//Hashtable hash = new Hashtable();
+		//hash.Add("kills", kills);
+		//PhotonNetwork.LocalPlayer.SetCustomProperties(hash);
+	}
+
+	//public static PlayerManager Find(Player player)
+	//{
+		//return FindObjectsOfType<PlayerManager>().SingleOrDefault(x => x.PV.Owner == player);
+	//}
 }
